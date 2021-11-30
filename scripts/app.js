f3f761b17891640bcaa9d0cc55667191e99d54e0
Christopher DeLeon <lionscantcode@gmail.com>
2021-11-30 15:14:20 -0600

    Fixed gitignore

--- scripts/app.js
+++ scripts/app.js
@@ -9,7 +9,7 @@ const api_key = config.NASA_API_KEY
 //Request using fetch method, await response and parse JSON response into JavaScript Object
 const fetchNasaData = async () => {
     try {
-        const response = await fetch(`${url}${process.env.NASA_API_KEY}`) // ES6 Template Concatination
+        const response = await fetch(`${url}${process.env.NASA_API_KEY}`) // ES6 Template Concatination - check this line if theres bugs
         const data = await response.json()
         console.log('NASA APOD API data', data)
     } catch (error) {
--- scripts/config.js
+++ scripts/config.js
@@ -1,7 +1,3 @@
-// Created By: Christopher DeLeon
-// Date: November 29, 2021
-
-
 const config = {
     NASA_API_KEY: '0lNJnYAXTnxCwTUhaj1bNAgSVVmUM1J0knpLPyTH' // Save Personal NASA API key
 }
