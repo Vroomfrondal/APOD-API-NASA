1e402585bc2f62360c4b97e7d62b38f750078bf6
Christopher DeLeon <lionscantcode@gmail.com>
2021-11-30 16:41:55 -0600

    Bug Fix
    Fixed const display function to display data on webpage
    
    Restyled container
    
    Added intro paragraph on index.html

--- scripts/app.js
+++ scripts/app.js
@@ -9,19 +9,21 @@ const api_key = config.NASA_API_KEY
 //Request using fetch method, await response and parse JSON response into JavaScript Object
 const fetchNasaData = async () => {
     try {
-        const response = await fetch(`${url}${process.env.NASA_API_KEY}`) // ES6 Template Concatination - check this line if theres bugs
+        const response = await fetch(`${url}${api_key}`)
         const data = await response.json()
-        console.log('NASA APOD API data', data)
+        console.log('NASA APOD data', data)
+        displayData(data);
     } catch (error) {
         console.log(error)
     }
-}
+};
 
 // Display data on our browser with function to update DOM
 const displayData = data => {
     document.getElementById('title').textContent = data.title
     document.getElementById('date').textContent = data.date
-    document.getElementById('picture').src = data.hdurl
-    document.getElementById('explanation').textContent = data.explanation
+    document.getElementById('picture').src = data.hdurl;
+    document.getElementById('explanation').textContent = data.explanation;
 }
-fetchNasaData
+
+fetchNasaData();
