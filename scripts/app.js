d1392c42aff17424959ea32c9424e7a53edffa41
Christopher DeLeon <lionscantcode@gmail.com>
2021-11-30 19:58:38 -0600

    Project Reset

--- scripts/app.js
+++ scripts/app.js
@@ -1,29 +1,60 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Created By: Christopher DeLeon
 // Date: November 29, 2021
 
 
 // Global Variables
-const url = 'https://api.nasa.gov/planetary/apod?api_key='
-const api_key = config.NASA_API_KEY
-
-//Request using fetch method, await response and parse JSON response into JavaScript Object
-const fetchNasaData = async () => {
-    try {
-        const response = await fetch(`${url}${api_key}`)
-        const data = await response.json()
-        console.log('NASA APOD data', data)
-        displayData(data);
-    } catch (error) {
-        console.log(error)
-    }
-};
-
-// Display data on our browser with function to update DOM
-const displayData = data => {
-    document.getElementById('title').textContent = data.title
-    document.getElementById('date').textContent = data.date
-    document.getElementById('picture').src = data.hdurl //data.hdurl
-    document.getElementById('explanation').textContent = data.explanation;
-}
-
-fetchNasaData()
+//const url = 'https://api.nasa.gov/planetary/apod?api_key='
+//const api_key = config.NASA_API_KEY
+//
+////Request using fetch method, await response and parse JSON response into JavaScript Object
+//const fetchNasaData = async () => {
+//    try {
+//        const response = await fetch(`${url}${api_key}`)
+//        const data = await response.json()
+//        console.log('NASA APOD data', data)
+//        displayData(data);
+//    } catch (error) {
+//        console.log(error)
+//    }
+//};
+//
+//// Display data on our browser with function to update DOM
+//const displayData = data => {
+//    document.getElementById('title').textContent = data.title
+//    document.getElementById('date').textContent = data.date
+//    document.getElementById('picture').src = data.hdurl //data.hdurl
+//    document.getElementById('explanation').textContent = data.explanation;
+//}
+//
+//fetchNasaData()
+//
