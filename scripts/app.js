2d3e39ec8599691b9d7704b71450faf7731a2631
Christopher DeLeon <lionscantcode@gmail.com>
2021-12-03 17:16:27 -0600

    Calendar Functionality app.js
    - Added functionality to calendar so user can pick a day and API will return media from that day

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,15 @@
 // Created By: Christopher DeLeon
 // Date: November 30th, 2021
 
+function nasaRequested() {
+    
 const url = 'https://api.nasa.gov/planetary/apod?api_key='
 const apiKey = config.NASA_API_KEY
 const title = document.querySelector("#title")
 const copyright = document.querySelector("#copyright")
 const mediaSection = document.querySelector("#media-section")
 const information = document.querySelector("#description")
-const dateInput = document.querySelector("#datepicker")
-
+const currentDate = new Date().toISOString().slice(0, 10); // this breaks the website
 
 const imageSection = `<a id="hdimg" href="" target="_blank">
 <div class="image-div"> 
@@ -17,22 +18,25 @@ const imageSection = `<a id="hdimg" href="" target="_blank">
 </a>`
 const videoSection = `<div class="video-div"> <iframe id="videoLink" src="" frameborder="0"></iframe></div>`
 
+let newDate = "&date=" + dateInput.value + "&";
+
 //Fetch data: method 1
 function fetchData() {
     try {
-        fetch(url+apiKey)
-        .then(response=>response.json())
-        .then(json=> {
+        fetch(url+apiKey+newDate)
+        .then(response => response.json())
+        .then(json => {
             console.log('NASA Data:', json)
-        displayData(json)
+            displayData(json)
         })
     } catch(error) {
         console.log(error)
     }
 }
-fetchData()
 
+// Display data object in Browser that is returned from API 
 function displayData(data) {
+
     title.innerHTML = data.title
 
     if(data.hasOwnProperty("copyright")) {
@@ -41,10 +45,9 @@ function displayData(data) {
         copyright.innerHTML = ""
     }
 
-   
-
-    let newDate = "&date=" + dateInput.value + "&";
-
+    dateInput.innerHTML = (data.date)
+    dateInput.max = (currentDate)
+    dateInput.min = ("1995-06-16")
 
     if(data.media_type == "video") {
         mediaSection.innerhtml = videoSection;
@@ -54,9 +57,19 @@ function displayData(data) {
         document.getElementById("hdimg").href = data.hdurl
         document.getElementById("image_of_the_day").src = data.url
     }
-
     information.innerHTML = data.explanation
 }
+fetchData()
+}
+
+// Check if calendar day has been inputted and update if so
+const dateInput = document.querySelector("#datepicker")
+dateInput.addEventListener('change', (e) => {
+    e.preventDefault();
+    nasaRequested();
+})
+
+nasaRequested().onload;
 
 
 
