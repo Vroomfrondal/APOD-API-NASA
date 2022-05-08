c16f5af79d65db229e378e1e4dde570e0d1448cc
Vroomfrondal <lionscantcode@gmail.com>
2022-05-07 20:15:31 -0500

    ES6 Refresh
    - let to const
    
    - updated to arrow functions
    
    - rearrange dynamic HTML elements inside of JS
    
    - add gitignore
    
    - .then callbacks to async/await

--- scripts/app.js
+++ scripts/app.js
@@ -1,106 +1,103 @@
-// Created By: Christopher DeLeon
-// Date: November 30th, 2021
-
-function nasaRequested() {
-    const url = "https://api.nasa.gov/planetary/apod?api_key="
+const nasaRequested = () => {
+    const url = 'https://api.nasa.gov/planetary/apod?api_key='
     const apiKey = config.NASA_API_KEY
-    const title = document.querySelector("#title")
-    const copyright = document.querySelector("#copyright")
-    const mediaSection = document.querySelector("#media-section")
-    const information = document.querySelector("#description")
-    let currentDate = new Date().toISOString().slice(0, 10)
+    const title = document.querySelector('#title')
+    const copyright = document.querySelector('#copyright')
+    const mediaSection = document.querySelector('#media-section')
+    const information = document.querySelector('#description')
+    const dateInput = document.querySelector('#datepicker')
+    const resetButtonEl = document.querySelector('#reset-button')
+    const randomButtonEl = document.querySelector('#random-day-generator')
+    const currentDate = new Date().toISOString().slice(0, 10)
+    const newDate = '&date=' + dateInput.value + '&'
     const imageSection = `<a id="hdimg" href="" target="_blank" rel="noopener">
                             <div class="image-div"> 
-                            <img id="image_of_the_day" src="" alt="image-by-nasa"> 
+                                <img id="image_of_the_day" src="" alt="image-by-nasa"> 
                             </div>
                           </a>`
-    const videoSection = `<div class="video-div"> <iframe id="videoLink" src="" frameborder="0"></iframe></div>`
-    let newDate = "&date=" + dateInput.value + "&"
+    const videoSection = `<div class="video-div"> 
+                            <iframe id="videoLink" src="" frameborder="0"></iframe>
+                          </div>`
 
-    //Fetch data: method 1
-    function fetchData() {
+    const fetchData = async () => {
         try {
-            fetch(url + apiKey + newDate)
-                .then((response) => response.json())
-                .then((json) => {
-                    console.log("NASA Data:", json)
-                    displayData(json)
-                })
+            const apiResponse = await fetch(url + apiKey + newDate)
+            if (apiResponse.status === 200) {
+                const data = await apiResponse.json()
+                console.log('NASA Data:', data)
+                displayData(data)
+            } else alert('Nasa seems to be having an issue with their servers right now.')
         } catch (error) {
             console.log(error)
+            alert(error)
         }
     }
 
     // Display data object in Browser that is returned from API
     const displayData = (data) => {
+        // Title, Date, and media-description
         title.innerHTML = data.title
-
-        if (data.code == 500) {
-            window.alert("Nasa seems to be having an issue with their server. Perhaps try another time.")
-            console.log("Stopped nasa requests.")
-            return
-        }
-
-        if (data.hasOwnProperty("copyright")) {
-            copyright.innerHTML = data.copyright
-        } else {
-            copyright.innerHTML = "NASA"
-        }
-
         date.innerHTML = data.date
-        dateInput.max = currentDate
-        dateInput.min = "1995-06-16"
+        information.innerHTML = data.explanation
 
-        if (data.media_type == "video") {
+        // Ensure Calendar input only takes valid API days
+        dateInput.max = currentDate
+        dateInput.min = '1995-06-16'
+
+        // Media author credits if API has them
+        if (data.hasOwnProperty('copyright')) {
+            copyright.innerHTML = data.copyright
+        } else copyright.innerHTML = 'NASA'
+
+        // Check to see if API returns pic or video
+        if (data.media_type == 'video') {
             mediaSection.innerHTML = videoSection
-            document.getElementById("videoLink").src = data.url
+            document.getElementById('videoLink').src = data.url
         } else {
             mediaSection.innerHTML = imageSection
-            document.getElementById("hdimg").href = data.hdurl
-            document.getElementById("image_of_the_day").src = data.url
+            document.getElementById('hdimg').href = data.hdurl
+            document.getElementById('image_of_the_day').src = data.url
         }
-        information.innerHTML = data.explanation
     }
-    fetchData()
 
-    // mutate fetchData() to fetch API object with random date when HTML button is pressed (instead of todays date)
-    document.querySelector("#random-day-generator").addEventListener("click", () => {
-        function fetchData() {
-            //utility function to generate a random date after 2010 (before 2010 causes bugs)
-            function randomDate(start, end) {
+    randomButtonEl.addEventListener('click', () => {
+        const fetchData = async () => {
+            // A random date after 2010 (before 2010 causes API errors)
+            const randomDate = (start, end) => {
                 return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()))
             }
             let randomRolledDate = randomDate(new Date(2010, 0, 1), new Date())
-            randomRolledDate = "&date=" + randomRolledDate.toISOString().slice(0, 10) + "&"
 
+            randomRolledDate = '&date=' + randomRolledDate.toISOString().slice(0, 10) + '&'
+
+            // fetch with a random date instead of today.
             try {
-                //new API response
-                fetch(url + apiKey + randomRolledDate)
-                    .then((response) => response.json())
-                    .then((json) => {
-                        console.log(`Fetching random day...`)
-                        console.log("NASA Data:", json)
-                        displayData(json)
-                    })
+                const apiResponse = await fetch(url + apiKey + randomRolledDate)
+                if (apiResponse.status === 200) {
+                    const data = await apiResponse.json()
+                    console.log('NASA Data:', data)
+                    displayData(data)
+                } else alert('Nasa seems to be having an issue with their servers right now.')
             } catch (error) {
                 console.log(error)
+                alert(error)
             }
         }
         fetchData()
     })
 
-    // Call current day on reset-button click
-    document.getElementById("reset-button").addEventListener("click", () => {
-        console.log("Fetching today...")
+    resetButtonEl.addEventListener('click', () => {
+        console.log('Fetching today...')
         fetchData()
     })
-}
 
-// Check if calendar day has been inputted and update if so
-const dateInput = document.querySelector("#datepicker")
-dateInput.addEventListener("change", (e) => {
-    e.preventDefault()
-    nasaRequested()
-})
+    // Update if user enters specific calendar day
+    dateInput.addEventListener('change', (e) => {
+        e.preventDefault()
+        nasaRequested()
+    })
+
+    fetchData()
+}
 
 nasaRequested()
